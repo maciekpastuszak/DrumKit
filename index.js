fa5330ae686ef18d4183fb43ccbe71b85a3d5a48
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-11-07 08:59:21 +0100

    connect keypress and click to drums

--- index.js
+++ index.js
@@ -3,24 +3,57 @@ var noOfDrumBtns = document.querySelectorAll(".drum").length;
 for (let i = 0; i < noOfDrumBtns; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-        var btnClicked = this.innerHTML;
-
-switch (btnClicked) {
-    case "w":
-        var tom1 = new Audio('/sounds/tom-1.mp3')
-        tom1.play();
-        break;
-    case "a":
-        var tom2 = new Audio('/sounds/tom-2.mp3')
-        tom2.play();
-        break;
-
-    default:
-        break;
-}
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);     
       
     })
-   
-   
 
 }
+
+document.addEventListener("keydown", function(e) {
+    makeSound(e.key);
+})
+
+function makeSound(key) {
+
+    switch (key) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+  
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+  
+      case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+  
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+  
+      case "j":
+        var snare = new Audio('sounds/crash.mp3');
+        snare.play();
+        break;
+  
+      case "k":
+        var crash = new Audio('sounds/snare.mp3');
+        crash.play();
+        break;
+  
+      case "l":
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+  
+  
+      default: console.log(key);
+  
+    }
+  }
